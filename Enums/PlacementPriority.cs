ae8af9c14ac2009cfa7df954ae71d9593c017a2c
vertigomaster <janoel777@gmail.com>
2025-02-03 18:44:38 -0600

    added PlacementPriority enum

---
+++ Enums/PlacementPriority.cs
@@ -0,0 +1,11 @@
+namespace IDEK.PCG.StructureGen.Core.Enums
+{
+    public enum PlacementPriority
+    {
+        Critical = 1000, 
+        Need = 800,
+        NiceToHave = 600,
+        Luxury = 400,
+        Unwanted = 0
+    }
+}
